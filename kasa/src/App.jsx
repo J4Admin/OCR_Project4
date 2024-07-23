5a212180830ee75c27b4702f5447332e2e8ed71a
J4Admin <159776479+J4Admin@users.noreply.github.com>
2024-07-23 18:50:09 +0200

    add: eslint.

---
+++ kasa/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "./components/Navbar.jsx";
+import "./App.scss";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
---
+++ kasa/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <nav>
+      <div className="navbar-logo">
+        <img src="src/assets/images/LOGO.png" alt="logo" />
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
