ab3cc3fa234e8637f6ce794ea38df6c7bfbe8ef7
J4Admin <159776479+J4Admin@users.noreply.github.com>
2024-07-30 20:57:21 +0200

    remove console.log

--- kasa/src/components/Main.jsx
+++ kasa/src/components/Main.jsx
@@ -3,7 +3,6 @@ import "../styles/Main.scss";
 
 function Main(props) {
   const children = props;
-  console.log("voici les props:", props);
   return <main className="main">{...props.children}</main>;
 }
 
